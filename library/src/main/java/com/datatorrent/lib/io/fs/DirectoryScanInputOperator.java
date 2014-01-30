187c255a828901c86a51cdb29e7428292f3bae87
RashmiDR <rashmi@datatorrent.com>
2014-01-29 17:22:58 -0800

    Squashed commit of the following:
    commit 6c5dd92237b36b00f38cd4979713cd36c98e9555
    Author: RashmiDR <rashmi@datatorrent.com>
    Date:   Wed Jan 29 17:20:30 2014 -0800
    
        directory scan input operator that emits file info records fixes MLHR#321

---
+++ library/src/main/java/com/datatorrent/lib/io/fs/DirectoryScanInputOperator.java
@@ -0,0 +1,287 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.io.fs;
+
+import com.datatorrent.api.ActivationListener;
+import com.datatorrent.api.BaseOperator;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.Context.OperatorContext;
+import com.datatorrent.api.InputOperator;
+
+import java.io.File;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.validation.constraints.NotNull;
+import org.apache.commons.io.FilenameUtils;
+import org.python.antlr.PythonParser.and_expr_return;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.commons.io.monitor.FileAlterationListener;
+import org.apache.commons.io.monitor.FileAlterationObserver;
+
+/**
+ *
+ */
+/**
+ * Input Adapter for scanning a specified directory
+ * <p>
+ * <br>
+ * 
+ * <br>
+ * 
+ * @param <STREAM>
+ * @since 0.9.3
+ */
+public class DirectoryScanInputOperator extends BaseOperator implements InputOperator, ActivationListener<OperatorContext>
+{
+  private File baseDirectory = null;
+
+  @NotNull
+  private static final Logger LOG = LoggerFactory.getLogger(DirectoryScanInputOperator.class);
+  private String directoryPath;
+  private int scanIntervalInMilliSeconds = 1000;// Default is 1 sec = 1000ms
+  private int fileCountPerEmit = 200;
+
+  public final transient DefaultOutputPort<FileInfoRecord> outport = new DefaultOutputPort<FileInfoRecord>();
+  private transient FileAlterationObserver observer;
+  private transient FAListener listner;
+
+  private LinkedBlockingQueue<FileInfoRecord> fileRecordsQueue = new LinkedBlockingQueue<FileInfoRecord>();
+
+  public class FAListener implements FileAlterationListener
+  {
+
+    // Is triggered when a file is created in the monitored folder
+    @Override
+    public void onFileCreate(File file)
+    {
+      LOG.info("onFileCreate : " + file);
+      FileInfoRecord fileRecord = new FileInfoRecord(file.getAbsolutePath(), FilenameUtils.getExtension(file.getAbsolutePath()), file.length());
+      // fileRecordsQueue.add(fileRecord);
+      try {
+        fileRecordsQueue.put(fileRecord);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public void onFileDelete(File file)
+    {
+      LOG.info("onFileDelete : " + file);
+    }
+
+    @Override
+    public void onDirectoryCreate(File directory)
+    {
+      LOG.info("onDirectoryCreate : " + directory);
+    }
+
+    @Override
+    public void onDirectoryDelete(File directory)
+    {
+      LOG.info("onDirectoryDelete : " + directory);
+    }
+
+    @Override
+    public void onDirectoryChange(File directory)
+    {
+      LOG.info("onDirectoryChange : " + directory);
+    }
+
+    @Override
+    public void onFileChange(File file)
+    {
+      LOG.info("onFileChange : " + file);
+    }
+
+    @Override
+    public void onStart(FileAlterationObserver arg0)
+    {
+      LOG.info("onStart of FileAlterationListener!!!!!");
+
+    }
+
+    @Override
+    public void onStop(FileAlterationObserver arg0)
+    {
+      LOG.info("onStop of FileAlterationListener!!!!!");
+    }
+  }
+
+  public class DirectoryScanner implements Runnable
+  {
+    public void run()
+    {
+      try {
+        while (true) {
+          observer.checkAndNotify();
+          // Sleep for Scan interval
+          Thread.sleep(scanIntervalInMilliSeconds);
+        }
+      } catch (InterruptedException e) {
+        return;
+      }
+    }
+  }
+
+  // FAListener listener = new FAListener();
+  Thread directoryScanThread = new Thread(new DirectoryScanner());
+
+  /**
+   * Sets the path of the directory to be scanned. The separators in path are replaced with File.separatorChar and the
+   * directory instance is created
+   * 
+   * @param dirPath
+   *          the path of the directory to be scanned
+   */
+  public void setDirectoryPath(String dirPath)
+  {
+    this.directoryPath = dirPath;
+
+    baseDirectory = new File(directoryPath.replace('/', File.separatorChar).replace('\\', File.separatorChar));
+
+    if (!baseDirectory.exists()) {
+      // Check if monitored folder exists
+      throw new RuntimeException("Directory not found: " + baseDirectory);
+    }
+  }
+
+  /**
+   * Returns the path of the directory being scanned.
+   * 
+   * @return directoryPath the path of directory being scanned.
+   */
+  public String getDirectoryPath()
+  {
+    return directoryPath;
+  }
+
+  /**
+   * Sets the time interval at which the directory is to be scanned
+   * 
+   * @param scanIntervalInMilliSeconds
+   *          the time interval for scanning the directory
+   */
+  public void setScanIntervalInMilliSeconds(int scanIntervalInMilliSeconds)
+  {
+    this.scanIntervalInMilliSeconds = scanIntervalInMilliSeconds;
+  }
+
+  /**
+   * Returns the interval at which the directory is being scanned.
+   * 
+   * @return scanIntervalInMilliSeconds the interval at which the directory is being scanned
+   */
+  public int getScanIntervalInMilliSeconds()
+  {
+    return scanIntervalInMilliSeconds;
+  }
+
+  /**
+   * Sets the number of file records to output in one emit cycle
+   * 
+   * @param fileCount
+   *          the number of file records to output per emit
+   */
+  public void setFileCountPerEmit(int fileCount)
+  {
+    this.fileCountPerEmit = fileCount;
+  }
+
+  /**
+   * Returns the number of file records that are output per emit.
+   * 
+   * @return fileCountPerEmit the number of file records that are output per emit
+   */
+  public int getFileCountPerEmit()
+  {
+    return fileCountPerEmit;
+  }
+
+  @Override
+  public void setup(OperatorContext context)
+  {
+
+  }
+
+  /**
+   * Initializes the directory scan monitoring mechanism. and starts the thread that periodically scans the directory
+   */
+  @Override
+  final public void activate(OperatorContext ctx)
+  {
+    // Start the directory monitor
+    observer = new FileAlterationObserver(baseDirectory);
+
+    listner = new FAListener();
+    observer.addListener(listner);
+
+    directoryScanThread.start();
+  }
+
+  /**
+   * Stops the thread that periodically scans the directory
+   */
+  @Override
+  final public void deactivate()
+  {
+    // Interrupt the directoryScanThread and wait for it to join
+    directoryScanThread.interrupt();
+    try {
+      directoryScanThread.join();
+    } catch (InterruptedException e) {
+      LOG.error(e.getMessage());
+    }
+  }
+
+  @Override
+  public void teardown()
+  {
+
+  }
+
+  /**
+   * Emits the file info records up-to a maximum of the specified number of file records per emit. Each file info record
+   * has the file name, file type {@link and_expr_return} file size.
+   */
+  @Override
+  public void emitTuples()
+  {
+    int count = fileCountPerEmit;
+    try {
+      while (!fileRecordsQueue.isEmpty() && count > 0) {
+        outport.emit(fileRecordsQueue.take());
+        count--;
+      }
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void beginWindow(long windowId)
+  {
+
+  }
+
+  @Override
+  public void endWindow()
+  {
+  }
+
+}
---
+++ library/src/test/java/com/datatorrent/lib/io/fs/DirectoryScanInputOperatorTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.io.fs;
+
+
+import java.io.File;
+import java.io.IOException;
+import com.datatorrent.lib.testbench.CollectorTestSink;
+import junit.framework.Assert;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+/**
+ * Units tests for DirectorySanInputOperator
+ */
+public class DirectoryScanInputOperatorTest
+{
+	// Sample text file path.
+	protected String dirName = "../library/src/test/resources/directoryScanTestDirectory";
+
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+  @Test
+	public void testDirectoryScan() throws InterruptedException, IOException
+	{
+		DirectoryScanInputOperator oper = new DirectoryScanInputOperator();
+		oper.setDirectoryPath(dirName);
+		oper.setScanIntervalInMilliSeconds(1000);
+		oper.activate(null);
+
+		CollectorTestSink sink = new CollectorTestSink();
+		oper.outport.setSink(sink);
+		
+		//Test if the existing files in the directory are emitted.
+		Thread.sleep(1000);
+		oper.emitTuples();
+    
+    Assert.assertTrue("tuple emmitted: " + sink.collectedTuples.size(), sink.collectedTuples.size() > 0);
+<<<<<<< HEAD
+		Assert.assertEquals(sink.collectedTuples.size(), 15);
+=======
+		Assert.assertEquals(sink.collectedTuples.size(), 10);
+>>>>>>> MLHR321DirectoryScanInputOperator
+		
+		
+		//Test if the new file added is detected
+		sink.collectedTuples.clear();
+		
+<<<<<<< HEAD
+		File oldFile = new File("../library/src/test/resources/directoryScanTestDirectory/DateWritable.java");
+=======
+		File oldFile = new File("../library/src/test/resources/directoryScanTestDirectory/testFile1.txt");
+>>>>>>> MLHR321DirectoryScanInputOperator
+		File newFile = new File("../library/src/test/resources/directoryScanTestDirectory/newFile.java");
+		FileUtils.copyFile(oldFile, newFile);
+
+		int timeoutMillis = 2000;
+    while (sink.collectedTuples.isEmpty() && timeoutMillis > 0) {
+      oper.emitTuples();
+      timeoutMillis -= 20;
+      Thread.sleep(20);
+    }
+    		
+    Assert.assertTrue("tuple emmitted: " + sink.collectedTuples.size(), sink.collectedTuples.size() > 0);
+    Assert.assertEquals(sink.collectedTuples.size(), 1);
+    
+    //clean up the directory to initial state
+    newFile.delete();    
+		
+    oper.deactivate();
+		oper.teardown();
+	}
+
+}
